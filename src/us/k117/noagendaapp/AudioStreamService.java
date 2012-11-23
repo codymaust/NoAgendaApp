0be9779f458a8cafd62ad04557f1ef79646eb019
Kevin Coakley <coakley@gmail.com>
2012-11-23 11:56:49 -0800

    Added CPU and WIFI wake locks to AudioStreamService.java

--- src/us/k117/noagendaapp/AudioStreamService.java
+++ src/us/k117/noagendaapp/AudioStreamService.java
@@ -4,17 +4,22 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.WifiLock;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.PowerManager;
 import android.util.Log;
 
 public class AudioStreamService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener {
 	//private static final String ACTION_PLAY = "com.example.action.PLAY";
 	MediaPlayer mMediaPlayer = null;
+	WifiLock wifiLock  = null;
     
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -33,6 +38,8 @@ public class AudioStreamService extends Service implements MediaPlayer.OnPrepare
 		mMediaPlayer.stop();
 		mMediaPlayer.release();
 		mMediaPlayer = null;
+		
+		wifiLock.release();
 	}
 	
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -67,9 +74,21 @@ public class AudioStreamService extends Service implements MediaPlayer.OnPrepare
     				Log.d(getClass().getName(), "Exception: " + ex.toString());
     				ex.printStackTrace();
     			}
+    			
+    			// Set a partial CPU wake lock 
+    			mMediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
             
+    			// Set a WIFI wake lock if playing streaming audio
+    			if (audioUrl.contains("http"))
+    			{
+    				// need to wifiLock.release(); if you pause or stop the audio
+        			wifiLock = ((WifiManager) getSystemService(Context.WIFI_SERVICE)).createWifiLock(WifiManager.WIFI_MODE_FULL, "mylock");
+        			wifiLock.acquire();        			
+    			}
+    			
     			mMediaPlayer.setOnPreparedListener(this);
     			mMediaPlayer.prepareAsync(); // prepare async to not block main thread
+    			
     		} else {
     			Log.d(getClass().getName(), "MediaPlayer is already playing");
     		}
