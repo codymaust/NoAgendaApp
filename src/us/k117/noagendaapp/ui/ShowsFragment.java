df8d59698bbad246391c7da57c103c14f76f0afb
Kevin Coakley <coakley@gmail.com>
2012-12-28 20:14:37 -0800

    Added Context Menu to the Shows ListFragment

--- src/us/k117/noagendaapp/ui/ShowsFragment.java
+++ src/us/k117/noagendaapp/ui/ShowsFragment.java
@@ -6,9 +6,14 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.SimpleCursorAdapter;
 
 import us.k117.noagendaapp.R;
@@ -19,6 +24,9 @@ import us.k117.noagendaapp.db.ShowsTable;
 public class ShowsFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
 	private SimpleCursorAdapter adapter;
+	private static final int PLAY_ID = Menu.FIRST + 1;
+	private static final int DOWNLOAD_ID = Menu.FIRST + 2;
+	private static final int DELETE_ID = Menu.FIRST + 3;
 	
 	//
 	// Required for ListFragment (onCreate, onActivityCreated, onCreateView)
@@ -27,12 +35,14 @@ public class ShowsFragment extends ListFragment implements LoaderManager.LoaderC
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		fillData();
+		fillData();		
 	}
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {		
 		super.onActivityCreated(savedInstanceState);
+		// Attach the context menu to the ListFragment
+		registerForContextMenu(getListView());
 	}
 
 	@Override
@@ -41,6 +51,32 @@ public class ShowsFragment extends ListFragment implements LoaderManager.LoaderC
 		return view;
 	}
 	
+	//
+	// Required for Context Menu (onCreateContextMenu, onContextitemSeleted)
+	//
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+			menu.add(0, PLAY_ID, 0, R.string.shows_context_play);
+			menu.add(0, DOWNLOAD_ID, 1, R.string.shows_context_download);
+			menu.add(0, DELETE_ID, 2, R.string.shows_context_delete);
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case PLAY_ID:
+			Log.d(getClass().getName(), "PLAY");
+			return true;
+		case DOWNLOAD_ID:
+			Log.d(getClass().getName(), "DOWNLOAD");
+			return true;
+		case DELETE_ID:
+			Log.d(getClass().getName(), "DELETE");
+			return true;
+		}
+		return super.onContextItemSelected(item);
+	}
 	
 	//
 	// Required for LoaderManager.LoaderCallbacks<Cursor> (onCreateLoader, onLoadFinished, onLoaderReset)
