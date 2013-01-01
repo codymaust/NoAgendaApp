5efbd17318801491e39f0cffb252883528707a80
Kevin Coakley <coakley@gmail.com>
2013-01-01 12:04:56 -0800

    Added basic player fragment UI

---
+++ src/us/k117/noagendaapp/ui/PlayerFragment.java
@@ -0,0 +1,31 @@
+package us.k117.noagendaapp.ui;
+
+import us.k117.noagendaapp.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlayerFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.e("Test", "hello");
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_player, container, false);
+		return view;
+	}
+	
+}
