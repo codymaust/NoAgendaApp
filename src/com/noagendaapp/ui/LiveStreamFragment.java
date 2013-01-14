cc26abdf659da6b0dbf6ddf2ec2b0985cba96147
Kevin Coakley <coakley@gmail.com>
2013-01-13 16:52:08 -0800

    Removed unused logging

--- src/com/noagendaapp/ui/LiveStreamFragment.java
+++ src/com/noagendaapp/ui/LiveStreamFragment.java
@@ -3,7 +3,6 @@ package com.noagendaapp.ui;
 import com.noagendaapp.R;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +13,6 @@ public class LiveStreamFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.e("Test", "hello");
 	}
 
 	@Override
@@ -26,8 +24,6 @@ public class LiveStreamFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_livestream, container, false);
-		//TextView textView = (TextView) view.findViewById(R.id.liveStreamText);
-		//textView.setText("Live Stream");
 		return view;
 	}
 }
