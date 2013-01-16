59a3b745606ea93483c68fb0d053901d24ea715f
Kevin Coakley <coakley@gmail.com>
2013-01-15 18:51:21 -0800

    Update to support the changes to AudioStreamingService

--- src/com/noagendaapp/audio/AudioIntentReceiver.java
+++ src/com/noagendaapp/audio/AudioIntentReceiver.java
@@ -1,8 +1,12 @@
 package com.noagendaapp.audio;
 
+import com.noagendaapp.MainActivity;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Message;
+import android.os.RemoteException;
 import android.util.Log;
 
 public class AudioIntentReceiver extends BroadcastReceiver {
@@ -12,9 +16,13 @@ public class AudioIntentReceiver extends BroadcastReceiver {
 		Log.d(getClass().getName(), "Got onRecieve");
 		if (intent.getAction().equals(android.media.AudioManager.ACTION_AUDIO_BECOMING_NOISY)) {
 			Log.d(getClass().getName(), "Headphones Unplugged");
-	          // signal your service to stop playback TODO: Maybe change this to pause audio instead of stop the audio?
-			Intent audioStreamService = new Intent (ctx, AudioStreamService.class);
-			ctx.stopService(audioStreamService);
+	        // signal your service to stop playback TODO: Maybe change this to pause audio instead of stop the audio?
+			try {  	  
+				Message msg = Message.obtain(null, AudioStreamService.MSG_STOP_AUDIO, 0, 0);								
+				MainActivity.myService.send(msg);
+			} catch (RemoteException e) {
+				Log.w(getClass().getName(), "Exception sending message", e);
+			}
 		}
 	}
 }
