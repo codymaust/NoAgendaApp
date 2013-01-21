5dc2a96a18a7a1c30a5211fcfa7f6f9910189892
Kevin Coakley <coakley@gmail.com>
2013-01-21 13:21:46 -0800

    Major Rewrite. Service always run while the applicaton is running. Created handlers to recieve communication from the client and to send messages back to client. Added SeekTo & JumpTo functions to seek forward and back through the audio. Created a runnable thread so the Service can send the currentposition and duration back to the client.

--- src/com/noagendaapp/audio/AudioStreamService.java
+++ src/com/noagendaapp/audio/AudioStreamService.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import com.noagendaapp.MainActivity;
 import com.noagendaapp.R;
 
-import android.app.Activity;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -15,82 +14,220 @@ import android.media.MediaPlayer;
 import android.media.AudioManager;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiManager.WifiLock;
-import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.PowerManager;
+import android.os.RemoteException;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class AudioStreamService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, AudioManager.OnAudioFocusChangeListener {
-	//private static final String ACTION_PLAY = "com.noagendaapp.action.PLAY";
+
 	private static final int NOTIFICATION_ID = 3333;
 	
-	MediaPlayer mMediaPlayer = null;
-	AudioManager mAudioManager = null;
-	WifiLock wifiLock  = null;
+	MediaPlayer myMediaPlayer = null;
+	AudioManager myAudioManager = null;
+	WifiLock myWifiLock  = null;
+    Handler myHandler = new Handler();
 	
+    private static boolean isRunning = false;
+    private static boolean isPlaying = false;
+    Messenger myClient = null; // Keeps track of all current registered clients.
+    final Messenger myMessenger = new Messenger(new IncomingHandler()); // Target we publish for clients to send messages to IncomingHandler.
+    
+    //
+    // Different types of messages of that the Service and send and receive
+    //
+    public static final int MSG_REGISTER_CLIENT = 1;
+    public static final int MSG_UNREGISTER_CLIENT = 2;
+    public static final int MSG_STOP_AUDIO = 11;
+    public static final int MSG_PLAY_FILE = 12;
+    public static final int MSG_SEEK_TO = 13;
+    public static final int MSG_JUMP_TO = 14;
+    public static final int MSG_UPDATE_FRAGMENT_PLAYER = 99;
+    
     
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
+        return myMessenger.getBinder();
 	}
-	
+	    	
 	@Override
 	public void onCreate() {
 		Log.d(getClass().getName(), "Service Created");
+		
+		isRunning = true;
 	}
 
 	@Override
 	public void onDestroy() {
 		Log.d(getClass().getName(), "Service Destroyed");
-		mMediaPlayer.stop();
-		mMediaPlayer.release();
-		mMediaPlayer = null;
-		
-		// Check if wifiLock is held and release it if so
-		if (wifiLock != null && wifiLock.isHeld())
-		{
-			wifiLock.release();
-			Log.d(getClass().getName(), "WIFI lock released");
-		}
+
+        isRunning = false;
 	}
 	
     public int onStartCommand(Intent intent, int flags, int startId) {
     	Log.d(getClass().getName(), "Service onStartCommand");
     	
-    	String title = intent.getStringExtra("title");
-    	String subtitle = intent.getStringExtra("subtitle");
+    	return START_STICKY;
+    }
+    	
+    // MediaPlayer.OnErrorListener 
+    public void initMediaPlayer() {
+    	Log.d(getClass().getName(), "MediaPlayer: initMediaPlayer");
+    	
+    	myMediaPlayer = new MediaPlayer();
+
+        myMediaPlayer.setOnErrorListener(this);
+    }
+   
+    // MediaPlayer.OnErrorListener 
+	@Override
+	public boolean onError(MediaPlayer mp, int what, int extra) {
+		// TODO Auto-generated method stub
+		Log.d(getClass().getName(), "MediaPlayer: onError");
+		return false;
+	}
+	
+    // Called when MediaPlayer is ready 
+    public void onPrepared(MediaPlayer player) {
+    	Log.d(getClass().getName(), "onPrepared: MediaPlayer is ready");
+    	
+        player.start();               
+    }
+	
+	// AudioManager.OnAudioFocusChangeListener 
+    public void onAudioFocusChange(int focusChange) {
+        switch (focusChange) {
+        case AudioManager.AUDIOFOCUS_GAIN:
+            // resume playback
+        	Log.d(getClass().getName(), "onAudioFocusChange: AudioManager.AUDIOFOCUS_GAIN");
+        	if (myMediaPlayer == null) {
+        		initMediaPlayer();
+        	}
+            else if (!myMediaPlayer.isPlaying()) {
+            	myMediaPlayer.start();
+            }
+            
+        	myMediaPlayer.setVolume(1.0f, 1.0f);
+            break;
+
+        case AudioManager.AUDIOFOCUS_LOSS:
+            // Lost focus for an unbounded amount of time: stop playback
+        	Log.d(getClass().getName(), "onAudioFocusChange: AudioManager.AUDIOFOCUS_LOSS");
+        	StopAudio();
+            break;
+
+        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+            // Lost focus for a short time, but we have to stop
+            // playback. We don't release the media player because playback
+            // is likely to resume
+        	Log.d(getClass().getName(), "onAudioFocusChange: AudioManager.AUDIOFOCUS_LOSS_TRANSIENT");
+        	if (myMediaPlayer != null) {
+	            if (myMediaPlayer.isPlaying()) {
+	            	myMediaPlayer.pause();
+	            }
+        	}
+            break;
+
+        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+            // Lost focus for a short time, but it's ok to keep playing
+            // at an attenuated level
+        	Log.d(getClass().getName(), "onAudioFocusChange: AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK");
+        	if (myMediaPlayer != null) {
+	            if (myMediaPlayer.isPlaying()) {
+	            	myMediaPlayer.setVolume(0.1f, 0.1f);
+	            }
+        	}
+            break;
+        }
+    }
     
+    //
+    // Handler of incoming messages from clients 
+    //
+    class IncomingHandler extends Handler { 
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case MSG_REGISTER_CLIENT:
+            	myClient = msg.replyTo;
+                break;
+            case MSG_UNREGISTER_CLIENT:
+            	myClient = null;
+                break;
+            case MSG_STOP_AUDIO:
+            	StopAudio();
+            	break;
+            case MSG_PLAY_FILE:
+            	StartAudio(msg.getData().getString("audioUrl"), msg.getData().getString("title"), msg.getData().getString("subtitle"));
+                break;
+            case MSG_SEEK_TO:
+            	SeekTo(msg.arg1);
+            	break;
+            case MSG_JUMP_TO:
+            	JumpTo(msg.arg1);
+            	break;	
+            default:
+                super.handleMessage(msg);
+            }
+        }
+    }
+    
+    public static boolean isRunning() {
+        return isRunning;
+    }
+    
+    public static boolean isPlaying() {  	
+    	return isPlaying;
+    }
+               
+    private void StartAudio(String audioUrl, String title, String subtitle)
+    {  
     	// Request audio focus from the AudioManager
-    	mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-    	int result = mAudioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+    	myAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+    	int result = myAudioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
     	
     	// If AudioManager gives audio focus...
     	if ( result == AudioManager.AUDIOFOCUS_GAIN )
     	{
     		Log.d(getClass().getName(), "Got audio focus");
-    	
-    	
-        //if (intent.getAction().equals(ACTION_PLAY)) {
-          
-    		// Get the url (local or internet) from the start service intent
-    		String audioUrl = intent.getStringExtra("audioUrl");
-    		Log.d(getClass().getName(), "Got Audio URL: " + audioUrl);
-    	
-    		// Initialize MediaPlayer
-    		mMediaPlayer = new MediaPlayer();
+    	              	
+    		if (myMediaPlayer == null) {
     		
-    		// Check if the service is already play a file TODO: mMediaPlayer.isPlaying() does't work
-    		if (mMediaPlayer.isPlaying() == false)
+    			Log.d(getClass().getName(), "[StartAudio] Initializing new MediaPlayer");
+    			// Initialize MediaPlayer
+    			myMediaPlayer = new MediaPlayer();
+    		
+    			// Set a partial CPU wake lock 
+    			myMediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        
+    			// Set a WIFI wake lock if playing streaming audio
+    			if (audioUrl.contains("http"))
+    			{
+    				// need to wifiLock.release(); if you pause or stop the audio
+    				myWifiLock = ((WifiManager) getSystemService(Context.WIFI_SERVICE)).createWifiLock(WifiManager.WIFI_MODE_FULL, "mylock");
+    				myWifiLock.acquire();
+    				Log.d(getClass().getName(), "[StartAudio] WIFI lock acquired");
+    			}
+
+    			myMediaPlayer.setOnPreparedListener(this);
+    		} else {
+    			Log.d(getClass().getName(), "[StartAudio] Resetting MediaPlayer");
+    			myMediaPlayer.reset();
+    		}
+    		
+    		// Check if the service is already play a file TODO: myMediaPlayer.isPlaying() does't work
+    		if (myMediaPlayer.isPlaying() == false)
     		{
-    			Log.d(getClass().getName(), "MediaPlayer is not playing");
+    			Log.d(getClass().getName(), "[StartAudio] MediaPlayer is not playing");
     			
     			// Set the DataSource url (local or internet)
     			try {
-    				mMediaPlayer.setDataSource(audioUrl);            
+    				Log.d(getClass().getName(), "[StartAudio] SetDataSource: " + audioUrl);
+    				myMediaPlayer.setDataSource(audioUrl);            
     			} catch (IllegalArgumentException ex) {
     				Log.d(getClass().getName(), "Exception: " + ex.toString());
     				ex.printStackTrace();
@@ -103,25 +240,12 @@ public class AudioStreamService extends Service implements MediaPlayer.OnPrepare
     			} catch (Exception ex) {
     				Log.d(getClass().getName(), "Exception: " + ex.toString());
     				ex.printStackTrace();
-    			}
-    			
-    			// Set a partial CPU wake lock 
-    			mMediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-            
-    			// Set a WIFI wake lock if playing streaming audio
-    			if (audioUrl.contains("http"))
-    			{
-    				// need to wifiLock.release(); if you pause or stop the audio
-        			wifiLock = ((WifiManager) getSystemService(Context.WIFI_SERVICE)).createWifiLock(WifiManager.WIFI_MODE_FULL, "mylock");
-        			wifiLock.acquire();
-        			Log.d(getClass().getName(), "WIFI lock acquired");
-    			}
-    			
-    			mMediaPlayer.setOnPreparedListener(this);
-    			mMediaPlayer.prepareAsync(); // prepare async to not block main thread
+    			}    			  			
+
+    			myMediaPlayer.prepareAsync(); // prepare async to not block main thread
     			
     		} else {
-    			Log.d(getClass().getName(), "MediaPlayer is already playing");
+    			Log.d(getClass().getName(), "[StartAudio] MediaPlayer is already playing");
     		}
     		
     		// When streaming starts add a foreground Notification
@@ -138,111 +262,109 @@ public class AudioStreamService extends Service implements MediaPlayer.OnPrepare
              .setOngoing(true)
              .build();
     		
-    		startForeground(NOTIFICATION_ID, notification);  		 		
-    		
-    		
-    		// Notify the startService call that the result was ok. TODO: Needs work.
-    		Bundle extras = intent.getExtras();
-    		if (extras != null) {
-    			Messenger messenger = (Messenger) extras.get("MESSENGER");
-    			Message msg = Message.obtain();
-    			msg.arg1 = Activity.RESULT_OK;
-    			try {
-    				messenger.send(msg);
-    			} catch (android.os.RemoteException e1) {
-    				Log.w(getClass().getName(), "Exception sending message", e1);
-    			}
+    		startForeground(NOTIFICATION_ID, notification);  
+    	
+    		if (audioUrl.contains("http") == false)
+    		{
+    			myHandler.postDelayed(mUpdateTimeTask, 1000);
     		}
-    	//}
+            isPlaying = true;
+    			    	
     	} else {
-    		Log.d(getClass().getName(), "Could not get audio focus");
+    		Log.d(getClass().getName(), "[StartAudio] Could not get audio focus");
     	}
-    	
-    	return START_STICKY;
-    }
-
-    /** Called when MediaPlayer is ready */
-    public void onPrepared(MediaPlayer player) {
-    	Log.d(getClass().getName(), "onPrepared: MediaPlayer is ready");
-        player.start();
-    }
-
-    /** MediaPlayer.OnErrorListener **/
-    public void initMediaPlayer() {
-    	Log.d(getClass().getName(), "MediaPlayer: initMediaPlayer");
-    	
-    	mMediaPlayer = new MediaPlayer();
-
-        mMediaPlayer.setOnErrorListener(this);
     }
     
-    /** MediaPlayer.OnErrorListener **/
-	@Override
-	public boolean onError(MediaPlayer mp, int what, int extra) {
-		// TODO Auto-generated method stub
-		Log.d(getClass().getName(), "MediaPlayer: onError");
-		return false;
-	}
-	
-	/* AudioManager.OnAudioFocusChangeListener */
-    public void onAudioFocusChange(int focusChange) {
-        switch (focusChange) {
-        case AudioManager.AUDIOFOCUS_GAIN:
-            // resume playback
-        	Log.d(getClass().getName(), "onAudioFocusChange: AudioManager.AUDIOFOCUS_GAIN");
-        	if (mMediaPlayer == null) {
-        		initMediaPlayer();
-        	}
-            else if (!mMediaPlayer.isPlaying()) {
-            	mMediaPlayer.start();
-            }
-            
-        	mMediaPlayer.setVolume(1.0f, 1.0f);
-            break;
-
-        case AudioManager.AUDIOFOCUS_LOSS:
-            // Lost focus for an unbounded amount of time: stop playback and release media player
-        	Log.d(getClass().getName(), "onAudioFocusChange: AudioManager.AUDIOFOCUS_LOSS");
-        	if (mMediaPlayer != null) {
-	            if (mMediaPlayer.isPlaying())
-	            {
-	            	mMediaPlayer.stop();
-	            }
-	
-	            mMediaPlayer.release();
-	            mMediaPlayer = null;
-	
-	            // Check if wifiLock is held and release it if so
-	    		if (wifiLock.isHeld())
-	    		{
-	    			wifiLock.release();
-	    			Log.d(getClass().getName(), "WIFI lock released");
-	    		}
-        	}
-            break;
-
-        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-            // Lost focus for a short time, but we have to stop
-            // playback. We don't release the media player because playback
-            // is likely to resume
-        	Log.d(getClass().getName(), "onAudioFocusChange: AudioManager.AUDIOFOCUS_LOSS_TRANSIENT");
-        	if (mMediaPlayer != null) {
-	            if (mMediaPlayer.isPlaying()) {
-	            	mMediaPlayer.pause();
-	            }
-        	}
-            break;
-
-        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-            // Lost focus for a short time, but it's ok to keep playing
-            // at an attenuated level
-        	Log.d(getClass().getName(), "onAudioFocusChange: AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK");
-        	if (mMediaPlayer != null) {
-	            if (mMediaPlayer.isPlaying()) {
-	            	mMediaPlayer.setVolume(0.1f, 0.1f);
-	            }
-        	}
-            break;
-        }
+    private void StopAudio() {
+    	// if the MediaPlayer has been initialize then release it
+    	if ( myMediaPlayer != null ) {
+    		
+    		// Stop the MediaPlayer if audio is playing
+    		if (myMediaPlayer.isPlaying())
+    		{
+    			myMediaPlayer.stop();
+    		}
+		
+    		// release the MediaPlayer
+    		myMediaPlayer.release();
+			myMediaPlayer = null;
+    	}
+		
+    	// Remove the service from the foreground
+		stopForeground(true);
+    	
+		// Check if wifiLock is held and release it if so
+		if (myWifiLock != null && myWifiLock.isHeld())
+		{
+			myWifiLock.release();
+			Log.d(getClass().getName(), "WIFI lock released");
+		}
+		
+		// Stop the repeating runable thread
+		myHandler.removeCallbacks(mUpdateTimeTask);
+		
+		// Set the isPlaying flag to false
+		isPlaying = false;
     }
+    
+    private void SeekTo(int seekValue) {
+    	// Make sure myMediaPlayer has been created
+    	if (myMediaPlayer != null) {
+    		// Make sure myMediaPlayer is playing
+    		if (myMediaPlayer.isPlaying()) {
+    			// set the seekPosition; seekValue = time in milliseconds
+    			int seekPosition = myMediaPlayer.getCurrentPosition() + seekValue;
+    			
+    			// Make sure the seekPosition is valid
+    			if (seekPosition < myMediaPlayer.getDuration() && seekPosition > 0) {
+    				// Seek to the seekPosition
+    				myMediaPlayer.seekTo(seekPosition);
+    			}
+    		}
+    	}
+    }
+    
+    private void JumpTo(int seekValue) {
+    	// Make sure myMediaPlayer has been created
+    	if (myMediaPlayer != null) {
+    		// Make sure myMediaPlayer is playing
+    		if (myMediaPlayer.isPlaying()) {
+    			// set the seekPosition; is the % complete
+    			int seekPosition = (int) Math.round(myMediaPlayer.getDuration() * ( seekValue * .01));
+    			    			  			
+    			// Make sure the seekPosition is valid
+    			if (seekPosition < myMediaPlayer.getDuration() && seekPosition > 0) {
+    				// Seek to the seekPosition
+    				myMediaPlayer.seekTo(seekPosition);
+    			}
+    		}
+    	}
+    }
+    
+    // Background Runnable thread to run every second when audio is play to update the player information
+    private Runnable mUpdateTimeTask = new Runnable() {
+    	public void run() {
+    		if (myMediaPlayer != null) {
+    			
+        		if (myMediaPlayer.isPlaying()) {
+        			try {
+        				//Log.d(getClass().getName(), "(mUpdateTimeTask) Get Duration: " + String.valueOf(myMediaPlayer.getDuration()) + " Get Current Position: " + String.valueOf(myMediaPlayer.getCurrentPosition()) );
+        				// Send the currentPostion and duration to the client so the player can be updated
+        				myClient.send(Message.obtain(null, MSG_UPDATE_FRAGMENT_PLAYER, myMediaPlayer.getCurrentPosition(), myMediaPlayer.getDuration()));
+        			} catch (RemoteException e) {
+        				Log.w(getClass().getName(), "Exception sending message", e);
+        			}
+        			
+        		} else {
+        			Log.d(getClass().getName(), "(mUpdateTimeTask) myMediaPlayer is not playing");
+        		}
+    			
+        		// Wait one second before running again
+    			myHandler.postDelayed(this, 1000);
+    		} else {
+    			// Cancel the runnable thread if myMediaPlayer hasn't been initalized
+    			myHandler.removeCallbacks(mUpdateTimeTask);
+    		}
+    	}
+    };
 }
