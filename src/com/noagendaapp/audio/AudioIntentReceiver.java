4f38f91bc91520515d9e5629a53175778a811e99
Kevin Coakley <coakley@gmail.com>
2013-04-24 12:16:11 -0700

    Updated AudioIntentReciever to use the Stop function from MainActivity.activeEpisode

--- src/com/noagendaapp/audio/AudioIntentReceiver.java
+++ src/com/noagendaapp/audio/AudioIntentReceiver.java
@@ -5,8 +5,6 @@ import com.noagendaapp.MainActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Message;
-import android.os.RemoteException;
 import android.util.Log;
 
 public class AudioIntentReceiver extends BroadcastReceiver {
@@ -16,17 +14,8 @@ public class AudioIntentReceiver extends BroadcastReceiver {
 		Log.d(getClass().getName(), "Got onRecieve");
 		if (intent.getAction().equals(android.media.AudioManager.ACTION_AUDIO_BECOMING_NOISY)) {
 			Log.d(getClass().getName(), "Headphones Unplugged");
-	        // signal your service to stop playback TODO: Maybe change this to pause audio instead of stop the audio?
-			try {  	  
-				Message msg = Message.obtain(null, AudioStreamService.MSG_STOP_AUDIO, 0, 0);	
-
-				// Make sure the service is running before sending a message to it.
-				if (MainActivity.myService != null) {
-					MainActivity.myService.send(msg);
-				}
-			} catch (RemoteException e) {
-				Log.w(getClass().getName(), "Exception sending message", e);
-			}
+	        // signal your service to stop play back 
+			MainActivity.activeEpisode.Stop();
 		}
 	}
 }
