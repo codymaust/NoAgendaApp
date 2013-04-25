d04a7304b1709218c8a33cba78bbfc926aeee682
Kevin Coakley <coakley@gmail.com>
2013-04-24 20:47:34 -0700

    Suppressed some of the fields that ACRA reports when there is an error.

--- src/com/noagendaapp/MyApplication.java
+++ src/com/noagendaapp/MyApplication.java
@@ -4,9 +4,11 @@ import android.app.Application;
 
 import org.acra.*;
 import org.acra.annotation.*;
+import static org.acra.ReportField.*;
 
 @ReportsCrashes(formKey = "", // will not be used
 				formUri = "http://noagendaapp.com/errorlog.php",
+				customReportContent = { APP_VERSION_CODE, APP_VERSION_NAME, ANDROID_VERSION, PHONE_MODEL, CUSTOM_DATA, STACK_TRACE, LOGCAT },
 				mode = ReportingInteractionMode.TOAST,
 				forceCloseDialogAfterToast = false, // optional, default false
 				resToastText = R.string.acra_toast_text) 
