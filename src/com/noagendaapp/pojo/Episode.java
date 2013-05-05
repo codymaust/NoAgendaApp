codymaust/NoAgendaApp

commit 564f03975b41b370edd112c8daa267ca01d25ba4

Added the length, date and episode number variables from the RSS Free to the database and the Episode object

src/com/noagendaapp/pojo/Episode.java
@@ -1,6 +1,10 @@
 package com.noagendaapp.pojo;
 
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import com.noagendaapp.audio.AudioStreamService;
 import com.noagendaapp.db.EpisodeContentProvider;
@@ -33,6 +37,10 @@ public class Episode {
 	public String subtitle;
 	public String link;
 	public String position;
+	public String length;
+	public String dateText;
+	public Date date;
+	public String episodeNum;
 	public String audioUrl;
 	protected Boolean seekBarEnabled;
 	
@@ -43,7 +51,7 @@ public class Episode {
 		id = myId;
 		
 		// Get the Episode details from the sqlite database
-		String[] projection = { EpisodeTable.COLUMN_TITLE, EpisodeTable.COLUMN_SUBTITLE, EpisodeTable.COLUMN_LINK, EpisodeTable.COLUMN_POSITION };			
+		String[] projection = { EpisodeTable.COLUMN_TITLE, EpisodeTable.COLUMN_SUBTITLE, EpisodeTable.COLUMN_LINK, EpisodeTable.COLUMN_POSITION, EpisodeTable.COLUMN_LENGTH, EpisodeTable.COLUMN_DATE, EpisodeTable.COLUMN_EPISODE_NUM };			
 		Cursor myCursor = myActivity.getContentResolver().query(EpisodeContentProvider.CONTENT_URI, projection, EpisodeTable.COLUMN_ID + " = ?", new String[] { id }, null);
 
 		// Read the details from the sqlite database
@@ -52,7 +60,23 @@ public class Episode {
 			subtitle = myCursor.getString(myCursor.getColumnIndex(EpisodeTable.COLUMN_SUBTITLE));
 			link = myCursor.getString(myCursor.getColumnIndex(EpisodeTable.COLUMN_LINK));
 			position = myCursor.getString(myCursor.getColumnIndex(EpisodeTable.COLUMN_POSITION));
+			length = myCursor.getString(myCursor.getColumnIndex(EpisodeTable.COLUMN_LENGTH));
+			dateText = myCursor.getString(myCursor.getColumnIndex(EpisodeTable.COLUMN_DATE));
+			episodeNum = myCursor.getString(myCursor.getColumnIndex(EpisodeTable.COLUMN_EPISODE_NUM));			
 			audioUrl = GetLocalPath();
+		}
+		
+		//
+		// Convert the dateText from the database into a Date object
+		//
+		//<pubDate>Thu, 25 Apr 2013 15:06:55 -0500</pubDate>
+		SimpleDateFormat df = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+		
+		try {
+			date = df.parse(dateText);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			date = new Date();
 		}
 		
 		// Set GUI options

AndroidManifest.xml
@@ -2,7 +2,7 @@
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
     package="com.noagendaapp"
     android:versionCode="1"
-    android:versionName="2013.5.3" >
+    android:versionName="2013.5.4" >
 
     <uses-sdk
         android:minSdkVersion="14"

src/com/noagendaapp/db/EpisodeContentProvider.java
@@ -142,7 +142,7 @@ public class EpisodeContentProvider extends ContentProvider {
 	}
 	
 	private void checkColumns(String[] projection) {
-		String[] available = { EpisodeTable.COLUMN_TITLE, EpisodeTable.COLUMN_SUBTITLE, EpisodeTable.COLUMN_LINK, EpisodeTable.COLUMN_POSITION,EpisodeTable.COLUMN_ID };
+		String[] available = { EpisodeTable.COLUMN_TITLE, EpisodeTable.COLUMN_SUBTITLE, EpisodeTable.COLUMN_LINK, EpisodeTable.COLUMN_POSITION, EpisodeTable.COLUMN_LENGTH, EpisodeTable.COLUMN_DATE, EpisodeTable.COLUMN_EPISODE_NUM, EpisodeTable.COLUMN_ID };
 		
 		if (projection != null) {
 			HashSet<String> requestedColumns = new HashSet<String>(Arrays.asList(projection));

src/com/noagendaapp/db/EpisodeTable.java
@@ -12,15 +12,21 @@ public class EpisodeTable {
 	public static final String COLUMN_SUBTITLE = "subtitle";
 	public static final String COLUMN_LINK = "link";
 	public static final String COLUMN_POSITION = "position";
+	public static final String COLUMN_LENGTH = "length";
+	public static final String COLUMN_DATE = "date";
+	public static final String COLUMN_EPISODE_NUM = "episode_num";
 
 	// Database creation SQL statement
 	private static final String DATABASE_CREATE = "create table " + TABLE_EPISODE
 		+ "(" 
 		+ COLUMN_ID + " integer primary key autoincrement, " 
 		+ COLUMN_TITLE + " text not null, " 
-		+ COLUMN_SUBTITLE + " text not null," 
-		+ COLUMN_LINK + " text not null,"
-		+ COLUMN_POSITION + " integer default 0"
+		+ COLUMN_SUBTITLE + " text not null, " 
+		+ COLUMN_LINK + " text not null, "
+		+ COLUMN_POSITION + " integer default 0, "
+		+ COLUMN_LENGTH + " integer default 0, "
+		+ COLUMN_DATE + " text not null, "
+		+ COLUMN_EPISODE_NUM + " text not null " 
 		+ ");";
 
 	public static void onCreate(SQLiteDatabase database) {

src/com/noagendaapp/pojo/LiveStream.java
@@ -1,5 +1,7 @@
 package com.noagendaapp.pojo;
 
+import java.util.Date;
+
 import com.noagendaapp.R;
 
 import android.app.Activity;
@@ -13,6 +15,9 @@ public class LiveStream extends Episode {
 		subtitle = myActivity.getResources().getString(R.string.live_stream_subtitle);
 		link = "";
 		position = "0";
+		length = "0";
+		date = new Date();
+		episodeNum = "0";
 		audioUrl = myActivity.getResources().getString(R.string.live_stream_url);
 		
 		// Set GUI options

src/com/noagendaapp/rss/DownloadRSSTask.java
@@ -87,11 +87,17 @@ public class DownloadRSSTask extends AsyncTask<String, Void, String> {
 		String title = null;
 		String subtitle = null;
 		String link = null;
+		String length = null;
+		String date = null;
+		String episodeNum = null;
 		
     	for (Entry entry : entries) {
     		title = entry.title;
     		subtitle = entry.summary;
     		link = entry.link;
+    		length = entry.length;
+    		date = entry.date;
+    		episodeNum = entry.episodeNum;
     		
     		// Only save if either summary or description
     		// is available
@@ -103,6 +109,9 @@ public class DownloadRSSTask extends AsyncTask<String, Void, String> {
     		values.put(EpisodeTable.COLUMN_TITLE, title);
     		values.put(EpisodeTable.COLUMN_SUBTITLE, subtitle);
     		values.put(EpisodeTable.COLUMN_LINK, link);
+    		values.put(EpisodeTable.COLUMN_LENGTH, length);
+    		values.put(EpisodeTable.COLUMN_DATE, date);
+    		values.put(EpisodeTable.COLUMN_EPISODE_NUM, episodeNum);
 
     		// query the database for the episode link
     		String[] projection = { EpisodeTable.COLUMN_LINK };

src/com/noagendaapp/rss/RSSFeedXmlParser.java
@@ -12,7 +12,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class RSSFeedXmlParser {
 
     public List<Entry> parse(InputStream in) throws XmlPullParserException, IOException {
@@ -30,6 +29,9 @@ public class RSSFeedXmlParser {
             String title = null;
             String summary = null;
             String link = null;
+            String length = null;
+            String date = null;
+            String episodeNum = null;
 
             //
             // Loop though each parsing event of the RSS feed
@@ -49,22 +51,35 @@ public class RSSFeedXmlParser {
           			// XML attributes
                     if ("enclosure".equals(currentTag)) {
                         link = parser.getAttributeValue(null, "url");
+                        length = parser.getAttributeValue(null, "length");
                     }  
                 // Get xml text
               	} else if (parser.getEventType() == XmlPullParser.TEXT) {
               		
                     if (parser.getText() != null && !parser.getText().trim().isEmpty() && "title".equals(currentTag)) {
                         title = parser.getText();
+                        
+                        // Parse the title on - and get the episode number, the second element "NA-117-1978-11-07"
+                        try {
+                        	String[] titleArray = title.split("-");
+                        	episodeNum = titleArray[1];
+                        } catch (Exception ex) {
+                        	// TODO: Do something with this exception other than just assign the var ERR
+                        	episodeNum = "ERR";
+                        }
                     }
                     if (parser.getText() != null && !parser.getText().trim().isEmpty() && "itunes:subtitle".equals(currentTag)) {
                         summary = parser.getText();
+                    }
+                    if (parser.getText() != null && !parser.getText().trim().isEmpty() && "pubDate".equals(currentTag)) {
+                        date = parser.getText();
                     }
                 // Act when xml tags are closed    
                 } else if (parser.getEventType() == XmlPullParser.END_TAG) {
 
                 	// When the end of the item tag is reached then add a new Entry object to the entires array
                     if ("item".equals(parser.getName())) {
-                    	entries.add(new Entry(title, summary, link));
+                    	entries.add(new Entry(title, summary, link, length, date, episodeNum));
                     }
                 }
             }
@@ -81,11 +96,17 @@ public class RSSFeedXmlParser {
         public final String title;
         public final String link;
         public final String summary;
+        public final String length;
+        public final String date;
+        public final String episodeNum;
 
-        private Entry(String title, String summary, String link) {
+        private Entry(String title, String summary, String link, String length, String date, String episodeNum) {
             this.title = title;
             this.summary = summary;
             this.link = link;
+            this.length = length;
+            this.date = date;
+            this.episodeNum = episodeNum;
         }
     }
 }

commit 9cf3cfc597560ed774b7860a21571b9c5521a78e

Fixed bug introduced in 564f03975b41b370edd112c8daa267ca01d25ba4 that caused the Live Stream to fail

src/com/noagendaapp/pojo/Episode.java
@@ -1,7 +1,6 @@
 package com.noagendaapp.pojo;
 
 import java.io.File;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
@@ -38,7 +37,6 @@ public class Episode {
 	public String link;
 	public String position;
 	public String length;
-	public String dateText;
 	public Calendar date;
 	public String episodeNum;
 	public String audioUrl;
@@ -54,6 +52,8 @@ public class Episode {
 		String[] projection = { EpisodeTable.COLUMN_TITLE, EpisodeTable.COLUMN_SUBTITLE, EpisodeTable.COLUMN_LINK, EpisodeTable.COLUMN_POSITION, EpisodeTable.COLUMN_LENGTH, EpisodeTable.COLUMN_DATE, EpisodeTable.COLUMN_EPISODE_NUM };			
 		Cursor myCursor = myActivity.getContentResolver().query(EpisodeContentProvider.CONTENT_URI, projection, EpisodeTable.COLUMN_ID + " = ?", new String[] { id }, null);
 
+		String dateText = null;
+		
 		// Read the details from the sqlite database
 		if (myCursor.moveToFirst()) {
 			title = myCursor.getString(myCursor.getColumnIndex(EpisodeTable.COLUMN_TITLE));
@@ -69,16 +69,17 @@ public class Episode {
 		//
 		// Convert the dateText from the database into a Calendar object
 		//
-		//<pubDate>Thu, 25 Apr 2013 15:06:55 -0500</pubDate>
-		date = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+		// if dateText isn't null then set the date to the value of dateText
+		if ( dateText != null ) {
+			date = Calendar.getInstance();
+			SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
 		
-		try {
-			date.setTime(sdf.parse(dateText));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
+			try {
+				date.setTime(sdf.parse(dateText));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+			}
 		}
-		
 		// Set GUI options
 		seekBarEnabled = true;
 	}